0deb4fd0deb7a5c829eaa6c20574c3fabe898199
Sondos Reda <sondos.elghazawy@gmail.com>
2025-07-25 14:41:53 +0300

    new file:   Egyptify/src/Pages/auth/ForgotPassword.vue 	modified:   Egyptify/src/Pages/auth/login.vue 	modified:   Egyptify/src/routers/index.js

--- Egyptify/src/routers/index.js
+++ Egyptify/src/routers/index.js
@@ -3,15 +3,25 @@ import Login from '../Pages/auth/login.vue'
 import Hala from '../Pages/Hala.vue'
 import Home from '../Pages/home.vue'
 import Signup from '../Pages/auth/signup.vue'
+import Sara from '../Pages/sara.vue'
+import ForgotPassword from '../Pages/auth/ForgotPassword.vue'
+
    
 const routes = [
   { path: '/', component: Home },
   { path: '/login', component: Login },
+  { path: '/auth/Forgot-Password', component: ForgotPassword },
+
   {path: '/Signup', component: Signup},
   {
     path: '/Hala',
     name: 'Hala',
     component: Hala
+  },
+   {
+    path: '/Sara',
+    name: 'Sara',
+    component: Sara
   },
   
 ]
