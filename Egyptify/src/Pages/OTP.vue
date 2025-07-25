3fd934e52a9bbe5820163902f01d6b2ff2324980
yasmine <yasmineemad2023@gmail.com>
2025-07-25 14:52:35 +0300

    new file:   Egyptify/src/Pages/OTP.vue

---
+++ Egyptify/src/Pages/OTP.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full">
+    <Navbar /> 
+
+    <section class="p-8">
+
+
+
+    </section>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Navbar from '../components/navbar.vue'
+import Footer from "../components/footer.vue"
+import btn from "../components/BaseButton.vue"
+</script>
