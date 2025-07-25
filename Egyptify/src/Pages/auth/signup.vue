14d57483c4b102156ac7c55c8e93e8be7d4abc4d
Sondos Reda <sondos.elghazawy@gmail.com>
2025-07-25 11:39:54 +0300

    modified:   Egyptify/src/Pages/auth/login.vue 	new file:   Egyptify/src/Pages/auth/signup.vue 	new file:   Egyptify/src/components/AuthForm.vue 	modified:   Egyptify/src/components/InputField.vue 	deleted:    Egyptify/src/components/LoginForm.vue

---
+++ Egyptify/src/Pages/auth/signup.vue
@@ -0,0 +1,21 @@
+<!-- src/pages/auth/Signup.vue -->
+<template>
+  <div class="flex h-screen justify-center items-center bg-[#fefaf2] px-4">
+    <AuthForm
+      formTitle="Sign Up"
+      buttonLabel="Register"
+      :showUsername="true"
+      :showConfirmPassword="true"
+      :showForgotPassword="false"
+      @submit="handleSignup"
+    />
+  </div>
+</template>
+
+<script setup>
+import AuthForm from '../../components/AuthForm.vue'
+
+function handleSignup() {
+  alert('Signup submitted') // replace with actual logic
+}
+</script>
