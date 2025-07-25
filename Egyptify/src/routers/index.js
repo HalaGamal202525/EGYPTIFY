f27b8b9dd7e58d78f45fb094433688f74d32b274
manar esmat <manaresmat20@gmail.com>
2025-07-25 14:50:35 -0700

    I have added tabs and dropdown component

--- Egyptify/src/routers/index.js
+++ Egyptify/src/routers/index.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../Pages/auth/login.vue'
 import Hala from '../Pages/Hala.vue'
 import Home from '../Pages/home.vue'
+import Tabs from '../Pages/auth/tabs.vue'
+import DropdownMenu from '../Pages/auth/DropdownDemo.vue'
+
+
    
 const routes = [
   { path: '/', component: Home },
@@ -11,6 +15,14 @@ const routes = [
     name: 'Hala',
     component: Hala
   },
+  { path: '/tabs', component: Tabs },
+
+  {
+  path: '/dropdownDemo',
+  name: 'DropdownDemo',
+  component: DropdownMenu
+},
+
 ]
 
 const router = createRouter({
