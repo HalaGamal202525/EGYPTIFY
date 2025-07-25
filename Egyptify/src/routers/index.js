34011f533b044d0dce68c15a0e38d7716d627fe9
yasmine <yasmineemad2023@gmail.com>
2025-07-25 16:05:39 +0300

    modified:   Egyptify/src/routers/index.js

--- Egyptify/src/routers/index.js
+++ Egyptify/src/routers/index.js
@@ -5,15 +5,14 @@ import Home from '../Pages/home.vue'
 import Signup from '../Pages/auth/signup.vue'
 import Sara from '../Pages/sara.vue'
 import ForgotPassword from '../Pages/auth/ForgotPassword.vue'
-import OTP from '../Pages/OTP.vue'
+import otp from '../Pages/OTP.vue'
 
    
 const routes = [
   { path: '/', component: Home },
   { path: '/login', component: Login },
   { path: '/auth/Forgot-Password', component: ForgotPassword },
-  {
-     path: '/OTP', component: OTP },
+  {path: '/OTP', component: otp },
 
   {path: '/Signup', component: Signup},
   {
