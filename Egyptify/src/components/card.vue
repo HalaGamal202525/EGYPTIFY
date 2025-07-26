650fa8c846c01c9a23c00b5aad9d99201f861adf
Sarah Saeed <sarahmostfa0123@gmail.com>
2025-07-26 21:08:18 +0300

    Update card.vue

--- Egyptify/src/components/card.vue
+++ Egyptify/src/components/card.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="w-full max-w-[300px] bg-white rounded-xl shadow-md overflow-hidden flex flex-col">
-<div class="relative">
+<div class="w-full">
   <img :src="image" alt="Card image" class="h-48 w-full object-cover rounded-t-xl" />
-  
+
   <button
+   v-if="showHeart"
     @click="toggleFavorite"
     class="absolute top-2 right-2 w-9 h-9 rounded-full bg-white flex items-center justify-center shadow-md transition"
   >
     <i
+
       class="fa-heart fa-regular" style="color:#ffc340"
       :class="isFavorite ? 'text-red-500' : 'text-gray-400'"
     ></i>
@@ -16,10 +18,11 @@
 
     <div class="p-4 flex flex-col justify-between flex-1">
       <div>
-        <h2 class="text-lg font-bold text-gray-800">{{ title }}</h2>
-        <p class="text-sm text-gray-500 mb-2">{{ description }}</p>
+        <h2 class="text-lg font-bold text-gray-800 ">{{ title }}</h2>
+        <p class="text-sm text-gray-500 mb-2 py-2">{{ description }}</p>
 
-        <div v-if="rating" class="text-yellow-500 text-sm mb-1">
+
+        <div v-if="rating" class="text-yellow-500 text-sm mb-1 py-4">
           <span v-for="n in Math.floor(rating)" :key="n"><i class="fa-solid fa-star" style="color: #ffc340;"></i></span>
           <span v-if="rating % 1 !== 0"><i class="fa-solid fa-star-half-stroke" style="color: #ffc340;"></i></span>
         </div>
@@ -36,7 +39,7 @@
       <div class="flex items-center justify-between mt-4">
         <span class="text-yellow-600 font-bold text-md" v-if="price"> {{ price }} EGP</span>
 
- 
+
 <slot name="action" v-if="hasActionSlot" />
 <BaseButton v-else-if="showButton">{{ buttonText }}</BaseButton>
       </div>
@@ -72,6 +75,7 @@ const hasActionSlot = !!slots.action
 import { ref } from 'vue'
 
 const isFavorite = ref(false)
+const showHeart = ref(false)
 
 const toggleFavorite = () => {
   isFavorite.value = !isFavorite.value
