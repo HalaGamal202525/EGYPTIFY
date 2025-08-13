21f212753997369e372bcb80e3739e7828ef23f8
Sondos Reda <sondos.elghazawy@gmail.com>
2025-08-13 16:24:24 +0300

    modified:   Egyptify/src/routers/index.js

--- Egyptify/src/routers/index.js
+++ Egyptify/src/routers/index.js
@@ -275,9 +275,22 @@ component:FoodDetail
 
 ]
 
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes,
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    
+    if (savedPosition) {
+      return savedPosition
+    } else {
+    
+      return { top: 0 }
+    }
+  }
 })
+
 
 export default router
