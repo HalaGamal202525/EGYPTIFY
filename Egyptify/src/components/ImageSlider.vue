1b022ec2bd42fa20ba70a96f061d187fcc8053e6
Sarah Saeed <sarahmostfa0123@gmail.com>
2025-08-15 16:14:17 +0300

    modified:   Egyptify/api/Data/places.json 	modified:   Egyptify/package-lock.json 	modified:   Egyptify/package.json 	new file:   Egyptify/public/bookingRestaurants/ExplorePageHero.jpg 	new file:   Egyptify/public/icon/android-launchericon-192-192.png 	new file:   Egyptify/public/icon/android-launchericon-512-512.png 	new file:   Egyptify/public/icon/logo.png 	new file:   Egyptify/public/slider4.jpg 	modified:   Egyptify/src/Pages/DestinationPage.vue 	modified:   Egyptify/src/Pages/Transportation.vue 	modified:   Egyptify/src/components/ImageSlider.vue 	modified:   Egyptify/src/components/NavBar-Black.vue 	modified:   Egyptify/src/components/navbar.vue 	modified:   Egyptify/src/data/bookingEvents.json 	modified:   Egyptify/src/data/hotels.json 	modified:   Egyptify/vite.config.js

--- Egyptify/src/components/ImageSlider.vue
+++ Egyptify/src/components/ImageSlider.vue
@@ -1,11 +1,10 @@
 <template>
   <section class="slider-section">
-<Carousel :autoplay="3000" :loop="true" :wrapAround="true">
-  <Slide v-for="(img, index) in images" :key="index">
-    <img :src="img" class="slide-img" />
-  </Slide>
-</Carousel>
-
+    <Carousel :autoplay="3000" :loop="true" :wrapAround="true">
+      <Slide v-for="(img, index) in images" :key="index">
+        <img :src="img" class="slide-img" />
+      </Slide>
+    </Carousel>
 
     <div class="overlay">
       <div class="text-content">
@@ -19,24 +18,17 @@
 </template>
 
 <script setup>
-import { Carousel, Slide } from 'vue3-carousel'
-import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
 
 const images = [
-  // '/public/hero/40996b0ea58ec14d349bd07f20fac572.jpg',
-  // '/hero/bluehole.jpg',
-  // '/hero/luxor,aswan.webp',
-  //   '/hero/sailing.jpg',
-  // '/hero/seasunset.jpg'
-  '/public/hero/alazhar-mosque1.jpg',
-  '/public/hero/hero1.jpg',
-  '/src/assets/ExplorePageHero.jpg',
-  '/src/assets/slider/slider4.jpg',
-    '/hero/hero6.webp',
-   
-
-
-]
+ 
+  "/hero/alazhar-mosque1.jpg",
+  "/hero/hero1.jpg",
+  "/bookingRestaurants/ExplorePageHero.jpg",
+  "/slider4.jpg",
+  "/hero/hero6.webp",
+];
 // const images = Object.values(
 //   import.meta.glob('../assets/slider/*.jpg', { eager: true })
 // )
@@ -61,7 +53,6 @@ const images = [
   display: block;
 }
 
-
 .overlay {
   position: absolute;
   top: 0;
@@ -69,7 +60,7 @@ const images = [
   z-index: 10;
   width: 100%;
   height: 100%;
-  background-color: rgba(0,0,0,0.2);
+  background-color: rgba(0, 0, 0, 0.2);
   display: flex;
   justify-content: center;
   align-items: center;
