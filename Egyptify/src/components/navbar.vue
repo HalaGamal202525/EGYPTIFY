c899ba3acd545b89c60167a0806efa18038aee1b
Sarah Saeed <sarahmostfa0123@gmail.com>
2025-08-29 01:54:32 +0300

    modified:   Egyptify/src/Pages/history.vue 	modified:   Egyptify/src/components/navbar.vue

--- Egyptify/src/components/navbar.vue
+++ Egyptify/src/components/navbar.vue
@@ -1,10 +1,14 @@
 <template>
   <nav
-    class="w-full flex items-center justify-between px-[137px] py-4 h-20 
-           bg-transparent dark:bg-gray-900/80 absolute top-0 left-0 z-50 
-           transition-colors duration-300"
+    class="w-full flex items-center justify-between px-[137px] py-4 h-20 bg-transparent dark:bg-gray-900/80 absolute top-0 left-0 z-50 transition-colors duration-300"
   >
-    <div class="flex items-center">
+    <div class="flex items-center md:hidden">
+      <button @click="toggleMenu" class="text-white text-2xl">
+        <i class="fa-solid fa-bars"></i>
+      </button>
+    </div>
+
+    <div class="flex items-center ">
       <img
         src="../assets/logo.png"
         alt="Logo"
@@ -16,32 +20,27 @@
     <div class="hidden md:flex space-x-8 items-center text-[16px]">
       <a
         href="#"
-        class="px-4 text-white dark:text-gray-200 font-bold hover:text-yellow-400 
-               dark:hover:text-yellow-300 transition-colors"
+        class="px-4 text-white dark:text-gray-200 font-bold hover:text-yellow-400 dark:hover:text-yellow-300 transition-colors"
         @click="goTodestinations"
-      >Destinations</a
+        >Destinations</a
       >
       <a
         href="#"
-        class="px-4 text-white dark:text-gray-200 font-bold hover:text-yellow-400 
-               dark:hover:text-yellow-300 transition-colors"
+        class="px-4 text-white dark:text-gray-200 font-bold hover:text-yellow-400 dark:hover:text-yellow-300 transition-colors"
         @click="goTotripplaner"
-      >Trips</a
+        >Trips</a
       >
       <a
         href="#"
-        class="px-4 text-white dark:text-gray-200 font-bold hover:text-yellow-400 
-               dark:hover:text-yellow-300 transition-colors"
+        class="px-4 text-white dark:text-gray-200 font-bold hover:text-yellow-400 dark:hover:text-yellow-300 transition-colors"
         @click="goTotripreviews"
-      >Reviews</a
+        >Reviews</a
       >
 
       <div class="relative px-4" ref="moreButtonRef">
         <button
           @click="toggleMenu"
-          class="text-white dark:text-gray-200 font-bold hover:text-yellow-400 
-                  dark:hover:text-yellow-300 focus:outline-none cursor-pointer 
-                  transition-colors"
+          class="text-white dark:text-gray-200 font-bold hover:text-yellow-400 dark:hover:text-yellow-300 focus:outline-none cursor-pointer transition-colors"
         >
           More
         </button>
@@ -49,8 +48,7 @@
         <transition name="fade">
           <div
             v-if="isMenuOpen"
-            class="absolute top-full left-0 mt-2 bg-white dark:bg-gray-800 
-                    rounded shadow-md py-2 z-50 transition-colors"
+            class="absolute top-full left-0 mt-2 bg-white dark:bg-gray-800 rounded shadow-md py-2 z-50 transition-colors"
           >
             <Menubar :isOpen="true" />
           </div>
@@ -60,20 +58,31 @@
 
     <div class="flex items-center space-x-4">
       <button
-        @click="themeStore.toggleDarkMode" 
-        class="p-2 rounded-full bg-yellow-400 dark:bg-yellow-500 
-               hover:bg-yellow-500 dark:hover:bg-yellow-400 
-               transition-all duration-300 hover:scale-105"
-        :title="themeStore.isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'"
+        @click="themeStore.toggleDarkMode"
+        class="p-2 rounded-full bg-yellow-400 dark:bg-yellow-500 hover:bg-yellow-500 dark:hover:bg-yellow-400 transition-all duration-300 hover:scale-105"
+        :title="
+          themeStore.isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'
+        "
       >
-        <svg 
-          :class="['w-5 h-5 transition-transform', themeStore.isDarkMode ? 'rotate-180 text-black' : 'text-black']"
-          fill="currentColor" 
+        <svg
+          :class="[
+            'w-5 h-5 transition-transform',
+            themeStore.isDarkMode ? 'rotate-180 text-black' : 'text-black',
+          ]"
+          fill="currentColor"
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <path v-if="themeStore.isDarkMode" d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-          <path v-else fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd" />
+          <path
+            v-if="themeStore.isDarkMode"
+            d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+          />
+          <path
+            v-else
+            fill-rule="evenodd"
+            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+            clip-rule="evenodd"
+          />
         </svg>
       </button>
 
@@ -110,22 +119,33 @@
           :src="imageStore.selectedImage || '/about-us/girl-4.png'"
           :alt="user.displayName || 'User'"
           @click="toggleProfileMenu"
-          class="w-15 h-15 rounded-full object-contain border-2 border-yellow-400 
-                  dark:border-yellow-300 hover:scale-105 transition cursor-pointer"
+          class="w-15 h-15 rounded-full object-contain border-2 border-yellow-400 dark:border-yellow-300 hover:scale-105 transition cursor-pointer"
         />
         <transition name="fade">
-          <ul 
+          <ul
             v-if="isProfileMenuOpen"
             class="absolute right-0 mt-2 w-40 bg-white dark:bg-gray-800 shadow-lg rounded-md overflow-hidden z-50 text-black dark:text-gray-200"
           >
             <li>
-              <a @click="goToProfile" class="block px-4 py-2 hover:bg-yellow-200 dark:hover:bg-gray-700 cursor-pointer">Profile</a>
+              <a
+                @click="goToProfile"
+                class="block px-4 py-2 hover:bg-yellow-200 dark:hover:bg-gray-700 cursor-pointer"
+                >Profile</a
+              >
             </li>
             <li>
-              <a @click="goToHistory" class="block px-4 py-2 hover:bg-yellow-200 dark:hover:bg-gray-700 cursor-pointer">History</a>
+              <a
+                @click="goToHistory"
+                class="block px-4 py-2 hover:bg-yellow-200 dark:hover:bg-gray-700 cursor-pointer"
+                >History</a
+              >
             </li>
             <li class="border-t border-gray-200 dark:border-gray-700">
-              <a @click="logout" class="block px-4 py-2 hover:bg-red-200 dark:hover:bg-red-700 cursor-pointer text-red-500 dark:text-red-400">Logout</a>
+              <a
+                @click="logout"
+                class="block px-4 py-2 hover:bg-red-200 dark:hover:bg-red-700 cursor-pointer text-red-500 dark:text-red-400"
+                >Logout</a
+              >
             </li>
           </ul>
         </transition>
@@ -134,9 +154,7 @@
       <template v-else>
         <BaseButton
           @click="gologin"
-          class="bg-yellow-400 dark:bg-yellow-500 text-black font-bold px-4 py-2 
-                  rounded hover:bg-yellow-500 dark:hover:bg-yellow-400 
-                  hover:scale-105 transition cursor-pointer"
+          class="bg-yellow-400 dark:bg-yellow-500 text-black font-bold px-4 py-2 rounded hover:bg-yellow-500 dark:hover:bg-yellow-400 hover:scale-105 transition cursor-pointer"
         >
           Login
         </BaseButton>
@@ -223,9 +241,9 @@ const handleClickOutsideLang = (event) => {
 };
 
 const handleClickOutsideProfile = (event) => {
-  const profileButton = event.target.closest('img');
-  const profileMenu = event.target.closest('ul');
-  
+  const profileButton = event.target.closest("img");
+  const profileMenu = event.target.closest("ul");
+
   if (!profileButton && !profileMenu) {
     isProfileMenuOpen.value = false;
   }
@@ -241,7 +259,7 @@ function translatePage(lang) {
 }
 
 const toggleTheme = () => {
-  themeStore.toggleDarkMode(); 
+  themeStore.toggleDarkMode();
 };
 
 onMounted(() => {
@@ -280,7 +298,7 @@ function goTotripplaner() {
   router.push("/tripplanner");
 }
 function goTotripreviews() {
-  router.push("/user-review");
+  router.push("/site-review");
 }
 function goToProfile() {
   router.push("/profile");
